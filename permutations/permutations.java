521d6b7bdffa08bfd69f9660dc085116b8e18ea2
hanbi97 <rain7i@naver.com>
2021-05-28 20:27:13 +0900

    리스트 넣을때 new로 새로 만들어서 넣어줘야함, ref변수라 값 다 바뀜

--- permutations/permutations.java
+++ permutations/permutations.java
@@ -3,10 +3,9 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
-/*자바로 풀면 문제점: List, array 모두 연결되어있음..
+/*자바로 풀면 문제점: List, array 모두 연결되어있음.. 답에 넣을때 새로운 리스트 만들어줘야함
 새로 삽입되지 않고 기존 값만 바뀜
 자바 리스트: contains()로 값 있는지 확인 가능, 따로 불리언 변수 둘 필요 없음
-
 */
 class Solution {
 
@@ -19,15 +18,16 @@ class Solution {
 
     private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
        if(tempList.size() == nums.length){
-          list.add(new ArrayList<>(tempList));
-       } else{
+          list.add(new ArrayList<>(tempList)); //넣을때 새 리스트를 반드시 만들어줘야함
+          return;
+       }
           for(int i = 0; i < nums.length; i++){ 
              if(tempList.contains(nums[i])) continue; // element already exists, skip
              tempList.add(nums[i]);
              backtrack(list, tempList, nums);
              tempList.remove(tempList.size() - 1);
           }
-       }
+       
     } 
     
 }
